ba6e417b9d4be827e1c1fb41a0601c70b357c63b
DanTsk <tsekhmastryuk@gmail.com>
2018-10-22 00:57:44 +0300

    Shorhands for Route decorator

--- src/routing/controllers/controller.decorators.ts
+++ src/routing/controllers/controller.decorators.ts
@@ -77,9 +77,18 @@ export function Route(method: Method, path: string) {
 }
 
 
-export const Get: RouteHandlerDecorator = handlerFactory('get');
-export const Post: RouteHandlerDecorator = handlerFactory('post');
-export const Put: RouteHandlerDecorator = handlerFactory('put');
-export const Patch: RouteHandlerDecorator = handlerFactory('patch');
-export const Del: RouteHandlerDecorator = handlerFactory('delete');
+/* Short bindings using method names */
 export const All: RouteHandlerDecorator = handlerFactory('all');
+export const Get: RouteHandlerDecorator = handlerFactory('get');
+export const Put: RouteHandlerDecorator = handlerFactory('put');
+export const Del: RouteHandlerDecorator = handlerFactory('delete');
+export const Post: RouteHandlerDecorator = handlerFactory('post');
+export const Patch: RouteHandlerDecorator = handlerFactory('patch');
+
+/* Full route binding with alias to methods */
+export const RouteAll = (path: string) => Route(Method.ALL, path);
+export const RouteGet = (path: string) => Route(Method.GET, path);
+export const RoutePut = (path: string) => Route(Method.PUT, path);
+export const RouteDel = (path: string) => Route(Method.DELETE, path);
+export const RoutePost = (path: string) => Route(Method.POST, path);
+export const RoutePatch = (path: string) => Route(Method.PATCH, path);
