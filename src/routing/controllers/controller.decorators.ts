cea35d72667f7a3cbd7af037ef9eee31ff44b9bb
DanTsk <tsekhmastryuk@gmail.com>
2018-10-22 01:10:42 +0300

    Fixes for routing decorators

--- src/routing/controllers/controller.decorators.ts
+++ src/routing/controllers/controller.decorators.ts
@@ -72,7 +72,7 @@ export function Data(): ClassDecorator {
     return (target: any) => Reflect.defineMetadata(keys.DATA_CLASS, true, target);
 }
 
-export function Route(method: Method, path: string) {
+export function Route(method: Method, path: string = '/') {
     return (target: any, propertyKey: string | symbol) => Reflect.defineMetadata(keys.RAW_ROUTE, { method, path }, target, propertyKey);
 }
 
@@ -86,9 +86,9 @@ export const Post: RouteHandlerDecorator = handlerFactory('post');
 export const Patch: RouteHandlerDecorator = handlerFactory('patch');
 
 /* Full route binding with alias to methods */
-export const RouteAll = (path: string) => Route(Method.ALL, path);
-export const RouteGet = (path: string) => Route(Method.GET, path);
-export const RoutePut = (path: string) => Route(Method.PUT, path);
-export const RouteDel = (path: string) => Route(Method.DELETE, path);
-export const RoutePost = (path: string) => Route(Method.POST, path);
-export const RoutePatch = (path: string) => Route(Method.PATCH, path);
+export const RouteAll = (path?: string) => Route(Method.ALL, path);
+export const RouteGet = (path?: string) => Route(Method.GET, path);
+export const RoutePut = (path?: string) => Route(Method.PUT, path);
+export const RouteDel = (path?: string) => Route(Method.DELETE, path);
+export const RoutePost = (path?: string) => Route(Method.POST, path);
+export const RoutePatch = (path?: string) => Route(Method.PATCH, path);
