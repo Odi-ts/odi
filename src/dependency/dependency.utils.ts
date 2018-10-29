0444727f0299975905410b122afbaf1f206ca688
DanTsk <tsekhmastryuk@gmail.com>
2018-10-30 01:14:12 +0200

    Small fixes

--- src/dependency/dependency.utils.ts
+++ src/dependency/dependency.utils.ts
@@ -7,6 +7,21 @@ export const onInit = Symbol('onInit');
 
 export const autowiredPropsStore = new WeakMap<object, (string | symbol)[]>();
 
+
+function shortSet<T extends Class>(classType: T, id: string, type: "singleton" | "scoped", { props, constructorArgs }: TypelessComponentEntry<T>) {
+     /* Get other component versions */
+     const prevSettings = ComponentSettingsStorage.get(classType) || {};
+        
+     /* Already aliased like constructor */
+     ComponentSettingsStorage.set(classType, {
+         ...prevSettings, [id]: {  
+            type,
+            props,
+            constructorArgs: (constructorArgs as any)
+         } 
+     });
+}
+
 export function define<T extends Class> (classType: T) {
     const methods = {
         set(id: string, settings: ComponentEntry<T>) {
@@ -19,30 +34,12 @@ export function define<T extends Class> (classType: T) {
             return methods;
         },
         setSingleton(id: string, settings: TypelessComponentEntry<T>) {
-            /* Get other component versions */
-            const prevSettings = ComponentSettingsStorage.get(classType) || {};
-        
-            /* Already aliased like constructor */
-            ComponentSettingsStorage.set(classType, {
-                ...prevSettings, [id]: {  
-                    type: "singleton",
-                    props: settings.props,
-                    constructorArgs: (settings.constructorArgs as any)
-                } 
-            });
+            shortSet(classType, id, "singleton", settings);
+            return methods;
         },
         setScoped(id: string, settings: TypelessComponentEntry<T>) {
-            /* Get other component versions */
-            const prevSettings = ComponentSettingsStorage.get(classType) || {};
-        
-            /* Already aliased like constructor */
-            ComponentSettingsStorage.set(classType, {
-                ...prevSettings, [id]: {  
-                    type: "scoped",
-                    props: settings.props,
-                    constructorArgs: (settings.constructorArgs as any)
-                } 
-            });
+            shortSet(classType, id, "scoped", settings);
+            return methods;
         }
     }
 
