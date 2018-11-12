cfc8514911307b9cf2e5938151990b5081cb9adf
DanTsk <tsekhmastryuk@gmail.com>
2018-11-12 02:12:34 +0200

    Adding tests setup

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Hello function', () => {
+    it('should return hello world', () => {
+        const result = 'Hello World!';
+        expect(result).to.equal('Hello World!');
+    })
+});
