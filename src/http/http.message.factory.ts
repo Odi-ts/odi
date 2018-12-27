8cecc950b756e3f39731b808994f41aa3e29a7b3
DanTsk <tsekhmastryuk@gmail.com>
2018-12-28 00:13:12 +0200

    Add tests for Http Message

--- src/http/http.message.factory.ts
+++ src/http/http.message.factory.ts
@@ -10,12 +10,15 @@ const forbiddenMessage = 'You don`t have permissions to access / on this server'
 const empty = '';
 
 const statusCodes: { [id: number]: ICode} = {
-    200: {status : HttpStatus.Ok, message: empty},
+    200: {status: HttpStatus.Ok, message: empty},
     400: {status: HttpStatus.BadRequest, message: empty},
     403: {status: HttpStatus.Forbidden, message: forbiddenMessage},
     404: {status: HttpStatus.NotFound, message: empty}
 };
 
 export function createMessage(code: number, body: any) {
-    return new HttpMessage(code, statusCodes[code].status, body || statusCodes[code].message)
+    const message = statusCodes[code] ? statusCodes[code].status: '';
+    const payload =  body || (statusCodes[code] ? statusCodes[code].message : null)
+
+    return new HttpMessage(code, message, payload);
 }
---
+++ test/http/http.message.test.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { createMessage } from '../../src/http/http.message.factory';
+import { HttpMessage } from '../../src/http/http.message';
+import { HttpStatus } from '../../src/http/http.statuses';
+import { Ok, BadRequest, Forbidden, NotFound } from '../../src/http/index';
+
+describe('Http Message', () => {
+    describe('#HttpMessage(...)', () => {
+        const forbiddenMessage = 'You don`t have permissions to access / on this server';
+        const message = '';
+
+        const statusCodes: any = {
+            200: { status: HttpStatus.Ok, message },
+            400: { status: HttpStatus.BadRequest, message },
+            403: { status: HttpStatus.Forbidden, message: forbiddenMessage},
+            404: { status: HttpStatus.NotFound, message }
+        };
+
+        for(const code of [200, 400, 403, 404]) {
+            it(`should create ${code} http message with proper information`, () => {
+                const httpMessage = createMessage(code, null);
+
+                expect(httpMessage).to.be.instanceOf(HttpMessage);
+                expect(httpMessage.message).to.be.eq(statusCodes[code].status);
+                expect(httpMessage.body).to.be.eq(statusCodes[code].message);
+                expect(httpMessage.code).to.be.eq(code);
+            });
+        }
+    });
+
+    describe('#factory(...)', () => {
+        const sets = [
+            { func: Ok, code: 200 },
+            { func: BadRequest, code: 400 },
+            { func: Forbidden, code: 403 },
+            { func: NotFound, code: 404 },
+        ];
+
+        for(const { func, code } of sets) {
+            it(`should create correct message using ${func.name} function`, () => {
+                const message = func();
+
+                expect(message).to.be.instanceOf(HttpMessage);
+                expect(message.code).to.be.eq(code);
+            });
+        }
+    });
+});
